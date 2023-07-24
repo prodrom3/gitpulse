d8080846dcfc5402276a85c50860429951d9b111
prodrom3 <ioannis.tsavlidis@rwth-aachen.de>
2023-07-24 22:55:50 +0300

    Log files stored in a separate directory. The log files name includes now the date it run. The script now outputs at the end which repos were successfully updated

--- RepoRevamp.py
+++ RepoRevamp.py
@@ -1,16 +1,38 @@
 import os
 import subprocess
 import concurrent.futures
+import logging
+import datetime
 
 def update_repository(repo_path):
-    print(f"Updating: {repo_path}")
+    logging.info(f"Updating: {repo_path}")
     try:
         subprocess.run(["git", "pull"], cwd=repo_path, check=True)
-        print(f"Successfully updated: {repo_path}")
+        logging.info(f"Successfully updated: {repo_path}")
+        return repo_path
     except subprocess.CalledProcessError as e:
         print(f"Error updating {repo_path}: {e}")
 
+def setup_logging():
+    logs_directory = "logs"
+    if not os.path.exists(logs_directory):
+        os.makedirs(logs_directory)
+
+    log_file_name = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".log"
+    log_file_path = os.path.join(logs_directory, log_file_name)
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s [%(levelname)s]: %(message)s",
+        handlers=[
+            logging.StreamHandler(),
+            logging.FileHandler(log_file_path, mode="w")
+        ]
+    )
+
+
 def main():
+    setup_logging()
     root_directory = os.getcwd()
 
     repositories = []
@@ -18,8 +40,17 @@ def main():
         if ".git" in dirs:
             repositories.append(root)
 
+    updated_repositories = set()
     with concurrent.futures.ThreadPoolExecutor() as executor:
-        executor.map(update_repository, repositories)
+        results = executor.map(update_repository, repositories)
+        for result in results:
+            if result:
+                updated_repositories.add(result)
+
+    if updated_repositories:
+        print("\nRepositories successfully updated:")
+        for repo in updated_repositories:
+            print(repo)
 
 if __name__ == "__main__":
     main()
